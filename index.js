0e230f17f6673d90b40ba0309edb48600c3d3ba5
Craig Donvito <craigdonvito@MacBookPro.attlocal.net>
2025-01-15 09:20:18 -0500

    Adding in final formatting and comments

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //create prompt
-
 const userInput = prompt("Enter Froyo flavors separated by commas:");
 
 //userInput --> vanilla,vanilla,vanilla,strawberry,coffee,coffee
@@ -10,42 +9,37 @@ console.log("Order: " + order);
 
 //create function
 function orderQty(order) {
-  /** 
-    //create empty obj
-    //loop over array
-        //add the flavor as a key, with a value of 1 if the key doesn't exist otherwise add 1 to the current value
-            //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1
-    //return obj created above
-*/
 
-  const flavorCounts = {};
-  for (i = 0; i < order.length; i++) {
+  const flavorCounts = {}; //create empty object
+
+  for (i = 0; i < order.length; i++) { //iterate through the order to get flavor & qty
     let currentFlavor = order[i];
     let sameFlavor = 0;
     console.log("Current Flavor: " + currentFlavor);
-    sameFlavor = addOrder(currentFlavor, flavorCounts) 
-      if (sameFlavor === 1 ){
-        flavorCounts[currentFlavor]++;
-        console.log("Another order");
-      }
-      else if (sameFlavor === 0){
-        flavorCounts[currentFlavor] = 1;
-        console.log("New Flavor")
-      }
-      console.log(flavorCounts);
+    sameFlavor = addOrder(currentFlavor, flavorCounts); //determine if the current flavor is new or another order
+    if (sameFlavor === 1) { //if another order, add 1 to the qty of that flavor
+      flavorCounts[currentFlavor]++;
+      console.log("Another order");
+    } else if (sameFlavor === 0) { //if new flavor, add flavor with qty of 1
+      flavorCounts[currentFlavor] = 1;
+      console.log("New Flavor");
     }
+    //console.log(flavorCounts);
   }
 
-function addOrder (currentFlavor, flavorCounts) {
+  return flavorCounts; //return object with flavors and counts
+}
+
+function addOrder(currentFlavor, flavorCounts) {
   let sameFlavor = 0;
-  for (const flavor in flavorCounts) {
+  for (const flavor in flavorCounts) {//loop to determine if current flavor is new or not
     if (currentFlavor === flavor) {
-      console.log("Object Loop: " + flavor);
+      //console.log("Object Loop: " + flavor);
       sameFlavor = 1;
       break;
     }
   }
   return sameFlavor;
-};
+}
 
 console.log(orderQty(order));
